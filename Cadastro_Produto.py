a8b28808ca05b7945ba973f5a7055262c7ffc147
Vinicius Lara <157314993+ViniLara25@users.noreply.github.com>
2025-08-27 18:36:33 +0000

    Adionando margem de lucro

--- Cadastro_Produto.py
+++ Cadastro_Produto.py
@@ -1,5 +1,34 @@
-#Desenvolver um pequeno sistema para uma loja. Cadastro de produtos, calculo de prezo final (com impostos) e exibir o total.
+#Desenvolver um pequeno sistema para uma loja. Cadastro de produtos, calculo de preço final (com impostos) e exibir o total.
 #1. Pedir ao usuário o nome do produto.
-#2. Prezo de custo do produto.
+#2. Preço de custo do produto.
 #3. Porcentagem de lucro desejada.
 #4. Imposto sobre o valor de venda.
+
+#Cadastro do produto
+produto = ""
+while not produto: 
+    produto = input("Digite o produto: ").strip()
+if not produto: print("O produto não pode ficar em branco")
+print ("Item cadastrado com sucesso");
+
+#Valor do produto
+
+#valor_texto = ""
+#while not valor_texto:
+#    valor_texto = input("Digite o valor pago pelo produto: ").strip()
+#    valor_correto = valor_texto.replace(",",".")
+#    valor_pago = float(valor_correto)
+#if not valor_texto: print("O valor não pode ficar em branco")
+
+#print(f'O valor é: R${valor_pago:,.2f}')
+
+while True: 
+    try: 
+        valor_texto = input("Digite o valor do produto: ")
+        valor_correto = valor_texto.replace(',','.')
+        valor = float(valor_correto) 
+        print("Valor cadastrado com sucesso!")
+        break
+    except ValueError: print("Valor invalido, digite novamente.")
+
+#Margem de lucro.
