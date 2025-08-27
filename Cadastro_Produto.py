a311b30b37925cae3825058c9dfd1c72dd3d5a39
Vinicius Lara <157314993+ViniLara25@users.noreply.github.com>
2025-08-27 13:10:32 +0000

    Adicionando novo trabalho.

---
+++ Cadastro_Produto.py
@@ -0,0 +1,5 @@
+#Desenvolver um pequeno sistema para uma loja. Cadastro de produtos, calculo de prezo final (com impostos) e exibir o total.
+#1. Pedir ao usuário o nome do produto.
+#2. Prezo de custo do produto.
+#3. Porcentagem de lucro desejada.
+#4. Imposto sobre o valor de venda.
