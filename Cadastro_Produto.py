f60af4e2bb65f2377da7344d34d1673bf6e74686
Vinicius Lara <157314993+ViniLara25@users.noreply.github.com>
2025-08-27 19:38:53 +0000

    Finalizado.

--- Cadastro_Produto.py
+++ Cadastro_Produto.py
@@ -4,14 +4,14 @@
 #3. Porcentagem de lucro desejada.
 #4. Imposto sobre o valor de venda.
 
-#Cadastro do produto
+#Cadastro do produto.
 produto = ""
 while not produto: 
     produto = input("Digite o produto: ").strip()
-if not produto: print("O produto não pode ficar em branco")
-print ("Item cadastrado com sucesso");
+if not produto: print("O produto não pode ficar em branco\n")
+print ("Item cadastrado com sucesso\n");
 
-#Valor do produto
+#Valor pago pelo produto.
 
 #valor_texto = ""
 #while not valor_texto:
@@ -24,12 +24,12 @@ print ("Item cadastrado com sucesso");
 
 while True: 
     try: 
-        valor_texto = input("Digite o valor do produto: ")
+        valor_texto = input("Digite o valor pago pelo produto: R$")
         valor_correto = valor_texto.replace(',','.')
         valor = float(valor_correto) 
-        print("Valor cadastrado com sucesso!")
+        print("Valor cadastrado com sucesso!\n")
         break
-    except ValueError: print("Valor inválido, digite novamente.")
+    except ValueError: print("Valor inválido, digite novamente.\n")
 
 #Margem de lucro.
 
@@ -38,10 +38,23 @@ while True:
         lucro_texto = input("Digite a porcentagem de lucro: ")
         lucro_correto = lucro_texto.replace(',','.')
         lucro = float(lucro_correto)
-        print("Valor cadastrado com sucesso")
+        print("Valor cadastrado com sucesso\n")
         break
-    except ValueError: print("Valor inválido, digite novamente.")
+    except ValueError: print("Valor inválido, digite novamente.\n")
 
 #Calculos de venda com ICMS de São Paulo 18%.
 
+lucro_final = lucro/100
+imposto_icms = 0.18
+valor_lucro = valor*lucro_final
+valor_final = valor+valor_lucro
+valor_final_imposto = valor_final*imposto_icms
+valor_produto = valor_final-valor_final_imposto
+resultado = valor_produto-valor
 
+print(f"\nItem cadastrado: {produto}. \nValor pago pelo produto: R${valor}. \nMargem de lucro: {lucro}%. \nImposto ICMS sobre a venda do produto no estado de São Paulo: {imposto_icms*100}%. \n\nValor final de venda do produto: R${valor_produto:,.2f}")
+
+if valor_produto <= valor:
+    print(f"\nVocê terá prejuizo com este item de: R${resultado:,.2f}")
+else:
+    print(f"\nVocê tera ganho com a venda de: R${resultado:,.2f}")
